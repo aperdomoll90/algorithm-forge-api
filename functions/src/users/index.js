10afe946a7d2e061acbd4bfdb899e75afa80122f
aperdomoll90 <aperdomoll90@gmail.com>
2021-04-22 23:03:39 -0400

    functions get and add added

--- functions/src/users/index.js
+++ functions/src/users/index.js
@@ -1,32 +1,36 @@
-var admin = require("firebase-admin");
-var serviceAccount = require("../../credentials.json");
-
+const { response } = require('express');
+var admin = require('firebase-admin');
+var serviceAccount = require('../../credentials.json');
 
 let db;
 
 function connectToFB() {
-if(!db){
+  if (!db) {
     admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-    })
-    db = admin.firestore()
-}};
-
-
-exports.getAllUsers = (req,res) => {
-    connectToFB()
-    db.collection('users').get()
-    .then((userList)=>{
-        let allUsers =[]
-        userList.forEach(user => allUsers.push(user.data()))
-    })
-    res.send(allUsers)
+      credential: admin.credential.cert(serviceAccount),
+    });
+    db = admin.firestore();
+  }
 }
 
-exports.newUser = (req,res)=>{
-    connectToFB()
-    const newData = req.data
-    db.collection('users').add(newData)
-    .then(()=> this.getAllUsers(req,res))
-    .catch(err => res.send('Error creating new user', + err.message))
-}
+exports.getAllUsers = (req, res) => {
+  connectToFB();
+  db.collection('users')
+    .get()
+    .then((userList) => {
+      let allUsers = [];
+      userList.forEach((user) => {
+        allUsers.push(user.data());
+      });
+    });
+  res.send(allUsers);
+};
+
+exports.newUser = (req, res) => {
+  connectToFB();
+  const newData = req.data;
+  db.collection('users')
+    .add(newData)
+    .then(() => this.getAllUsers(req, res))
+    .catch((err) => res.send('Error creating new user', +err.message));
+};
